92faa9e5f4a8817b5d11903e4ca991d3c5155821
Alivop <alivop.liu@gmail.com>
2014-09-15 11:15:06 +0800

    fixed some problem

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-//LinePHP库目录,网站发布时一定要更改
-$t = microtime(true);
+//LinePHP library.When you publish your project,please change the value.
 define('LP_LIBRARY','lib');
 require_once(dirname(__FILE__).'/'.LP_LIBRARY.'/line/core/Bootstrap.php');
 Bootstrap::start();
-echo '<br/>',microtime(true)-$t;
